0aa39d92d7cc1bed298a2f61eb3a64cbf1c04971
Julliana Sousa <jullianasantossousa@hotmail.com>
2022-03-06 17:18:15 +0000

    Add responsiveness to the navbar

--- client/src/components/TheNavbar.vue
+++ client/src/components/TheNavbar.vue
@@ -1,66 +1,45 @@
 <template>
-  <nav class="navbar">
-    <div class="container-fluid d-flex justify-content-start">
-        <div>
-            <router-link to="/" class="navbar-brand">
-                <img alt="Logo do greenly" src="../assets/logo_dark.png">
-            </router-link>
-        </div>
-        <div class="input-group search-group mb-3">
-            <input class="form-control" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Pesquisar</button>
-        </div>
-        <div>
-            <router-link to="/login" class="float-right"> Iniciar Sessão </router-link>
-        </div>
-    </div>
-    <div class="nav-links mx-auto text-uppercase" >
-        <router-link to="/produtos" class="mb-2 mr-2">
-            produtos
-        </router-link>
-        <router-link to="/promocoes" class="mb-2 pr-2">
-            promoções
-        </router-link>
-        <router-link to="/servicos" class="mb-2 ml-2">
-            serviços
-        </router-link>
-        <router-link to="/fornecedores" class="mb-2 ml-2">
-            fornecedores
-        </router-link>
-    </div>
-  </nav>
-  <!-- <nav class="navbar">
+<nav>
     <div class="container">
-        <div class="row justify-content-md-center">
-            <div class="col-md-auto">
+        <div class="d-flex p-2">
+            <div class="align-self-center pt-3">
                 <router-link to="/" class="navbar-brand">
                     <img alt="Logo do greenly" src="../assets/logo_dark.png">
                 </router-link>
             </div>
-            <div class="col input-group search-group">
+            <div class="flex-grow-1 input-group search-group align-self-center">
                 <input class="form-control" type="search" placeholder="Search" aria-label="Search">
                 <button class="btn btn-outline-success" type="submit">Pesquisar</button>
             </div>
-            <div class="col">
-                 <router-link to="/login"> Iniciar Sessão </router-link>
+            <div class="p-2 align-self-center pt-4 text-uppercase nav-links">
+                <router-link to="/login" class="float-right"> Iniciar Sessão
+                </router-link>
+            </div>
+        </div>
+        <div class="d-flex justify-content-center pb-3 text-uppercase nav-links">
+            <div class="pt-2">
+                <router-link to="/produtos" class="mb-2 mr-2">
+                    produtos
+                </router-link>
+            </div>
+            <div class="pt-2">
+                <router-link to="/promocoes" class="mb-2 pr-2">
+                    promoções
+                </router-link>
+            </div>
+            <div class="pt-2">
+                <router-link to="/servicos" class="mb-2 ml-2">
+                    serviços
+                </router-link>
+            </div>
+            <div class="pt-2">
+                <router-link to="/fornecedores" class="mb-2">
+                    fornecedores
+                </router-link>
             </div>
         </div>
     </div>
-    <div class="nav-links mx-auto text-uppercase container">
-        <router-link to="/produtos" class="mb-2">
-            produtos
-        </router-link>
-        <router-link to="/promocoes" class="mb-2">
-            promoções
-        </router-link>
-        <router-link to="/servicos" class="mb-2">
-            serviços
-        </router-link>
-        <router-link to="/fornecedores" class="mb-2">
-            fornecedores
-        </router-link>
-    </div>
-  </nav> -->
+</nav>
 </template>
 
 <script>
@@ -80,6 +59,10 @@ export default {
         color: #fcfaf9;
         text-decoration: none;
         margin-left: 20px;
+    }
+
+    nav a:hover {
+        color: #dce5e1;
     }
 
     .nav-links {
