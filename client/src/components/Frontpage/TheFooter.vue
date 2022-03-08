fd069c247c59a774560a17f3ec9290ec4ce95f19
Julliana Sousa <jullianasantossousa@hotmail.com>
2022-03-08 00:27:39 +0000

    WIP: Add new infos to footer

--- client/src/components/Frontpage/TheFooter.vue
+++ client/src/components/Frontpage/TheFooter.vue
@@ -1,14 +1,55 @@
 <template>
-<div class="d-flex flex-column min-vh-100">
-    <footer class="bg-light text-center text-lg-start mt-auto">
-        <div class="text-center p-3 mt-auto footer">
-            Greenly © 2022
+<div class="d-flex align-self-stretch min-vh-100">
+    <footer class="align-self-stretch bg-light text-center text-lg-start mt-auto foot">
+        <div class="d-flex footer">
+            <div class="p-3 align-self-stretch ">
+                <p>Sobre nós</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor tempor
+                    nulla eget vestibulum. Ut orci urna, luctus vitae imperdiet id, commodo auctor
+                    diam. In non eleifend nunc. Nam ipsum purus, maximus eget est eget, ullamcorper
+                    congue nibh. Integer tempor, eros ut varius commodo, arcu nisl vehicula nunc,
+                    id mattis.
+                </p>
+            </div>
+            <div class="align-self-stretch p-3">
+                <p>Contacto</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor tempor
+                    nulla eget vestibulum. Ut orci urna, luctus vitae imperdiet id, commodo auctor
+                    diam. In non eleifend nunc. Nam ipsum purus, maximus eget est eget, ullamcorper
+                    congue nibh. Integer tempor, eros ut varius commodo, arcu nisl vehicula nunc,
+                    id mattis.
+                </p>
+            </div>
+            <div class="align-self-stretch p-3">
+                <p>Siga-nos</p>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi tempor tempor
+                    nulla eget vestibulum. Ut orci urna, luctus vitae imperdiet id, commodo auctor
+                    diam. In non eleifend nunc. Nam ipsum purus, maximus eget est eget, ullamcorper
+                    congue nibh. Integer tempor, eros ut varius commodo, arcu nisl vehicula nunc,
+                    id mattis.
+                </p>
+            </div>
+        </div>
+        <div class="d-flex p-3 footer">
+            <div class="align-self-stretcht">
+                <p>© 2022 Greenly <br> Todos os Direitos Reservados</p>
+            </div>
+            <div class="ms-auto">
+                 <font-awesome-icon :icon="['fab', 'cc-paypal']" size="2xl"/>
+            </div>
         </div>
     </footer>
 </div>
 </template>
 
 <script>
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faCcPaypal } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faCcPaypal);
 
 export default {
   name: 'TheFooter',
@@ -19,5 +60,10 @@ export default {
     .footer {
         background-color: #608072;
         color: #fcfaf9;
+    }
+
+    img {
+        width: 250px;
+        height: 105px;
     }
 </style>
