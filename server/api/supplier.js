125227eab7c24cc742c85b42491de219c2b92f77
fc55373 <fc55373@alunos.fc.ul.pt>
2022-03-09 23:18:26 +0000

    Built REST API for account management. Included all CRUD operations. Other improvements. - Persistence and route functions for main operations: creating, reading, updating and deleting; - Added validation for all API endpoints in this commit, using express-validator; - Added flags for request logging and database monitoring; - Added flag for SSL diversion during testing; - Added environment variable search on start; - Built skeleton for upcoming REST API routes; - Improved error handling; - Included and updated REST API OpenAPI specification; - Improved README.md; - Created database population script (multi-threaded) in /server/utils (functional), included respective requirements.txt; - Improved database structure (migrated datatypes, added fields, removed uniqueness); - Created validation library for route argument validation and error handling library; - Removed unused dependencies, added new dependencies; - Regenerated scheme.prisma.

---
+++ server/api/supplier.js
@@ -0,0 +1,11 @@
+/* Keep in mind, the path for each route in this file is prepended with /supplier */
+
+const express = require('express');
+const router = express.Router();
+
+/* Greenly libraries */
+const persistence = require('../lib/persistence.js')
+
+
+
+module.exports = router;
