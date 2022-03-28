d8ad4f72d09246a63621159bf5fdc78faef1374c
fc55373 <fc55373@alunos.fc.ul.pt>
2022-03-28 22:17:44 +0100

    Added authorization checking to protected routes: GET /user/, GET/PUT/DELETE /user/:userId.

--- server/api/user.js
+++ server/api/user.js
@@ -6,14 +6,15 @@ const { isAuthenticated } = require('../lib/authentication.js');
 const router = express.Router();
 
 /* Greenly libraries & required server data */
-const authentication = require("../lib/authentication")
-const persistence = require('../lib/persistence.js');
+const authentication    = require("../lib/authentication")
+const authorization     = require("../lib/authorization")
+const persistence       = require('../lib/persistence');
+const defaultErr        = require("../lib/error").defaultErr
 const { createUserValidator, updateUserValidator } = require('../lib/validation.js');
-const defaultErr = require("../lib/error").defaultErr
 
 /* GET /user (Admin only) */
 
-router.get('/', authentication.check, (req, res) => {
+router.get('/', authentication.check, authorization.check, (req, res) => {
     try {
         persistence.getAllUsers().then((users) => {
             res.status(200).json(users)
@@ -47,7 +48,7 @@ router.post('/', createUserValidator(), (req, res) => {
 
 /* GET /user/{userId} (User, Admin or Transporter only) */
 
-router.get('/:userId', authentication.check, (req, res, next) => {
+router.get('/:userId', authentication.check, authorization.check, (req, res, next) => {
     
     try {
         //TODO: With authentication and authorization in place, this DB call could be replaced with req.user
@@ -78,7 +79,7 @@ router.get('/:userId', authentication.check, (req, res, next) => {
 
 /* PUT /user/{userId} (User or Admin only) */
 
-router.put('/:userId', authentication.check, updateUserValidator(), (req, res) => {
+router.put('/:userId', authentication.check, authorization.check, updateUserValidator(), (req, res) => {
 
     try {
         persistence.updateUser(req.params.userId, req.body).then((success) => {
@@ -96,7 +97,7 @@ router.put('/:userId', authentication.check, updateUserValidator(), (req, res) =
 
 /* DELETE /user/{userId} (User or Admin only) */
 
-router.delete('/:userId', authentication.check, (req, res) => {
+router.delete('/:userId', authentication.check, authorization.check, (req, res) => {
 
     try {
         persistence.deleteUser(Number(req.params.userId)).then((success) => {
--- server/lib/authorization.js
+++ server/lib/authorization.js
@@ -1,3 +1,45 @@
 /*  Greenly Authorization Library
     Functions included pertain to user verification and resource limitation.
 */
+
+function check(req, res, next) {
+    // Identify resource type (locked routes only): User, Order, Warehouse, Distribution Center, Vehicle
+    const resourceIdentification = {"/user/": "ALL_USERS",
+                                    "/user/:userId": "SINGLE_USER",}
+
+    const intent = req.method
+    const incomingRoute = req.baseUrl + req.route.path
+    const isAdministrator = (user) => {return user.type == "ADMINISTRATOR"}
+
+    // TODO: Remove test lines
+    console.log(incomingRoute)
+
+    
+
+    // Interpreting resource from path
+    switch (resourceIdentification[incomingRoute]) {
+        case "ALL_USERS":
+            // Only an administrator can acccess information regarding all users    
+            if (isAdministrator(req.user)) return next();
+            break;
+    
+        case "SINGLE_USER":
+            // Besides the user himself, only administrators and related transporters/suppliers can access and manipulate specific information
+            // This is valid for: GET, PUT, DELETE
+            if ((req.params.userId == user.id) ||
+                (isAdministrator(req.user))) {
+                return next();
+            }
+
+            // TODO: Complete part regarding related transporters and suppliers (e.g. have an order in common)
+
+
+            break;
+        default:
+            break;
+    }
+
+    return res.status(403).send({message: "Insufficient permissions for specified resource."})
+}
+
+module.exports = {check}
