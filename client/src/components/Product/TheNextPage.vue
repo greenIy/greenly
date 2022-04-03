b9539ddb1489753083ce61660d624da763d8e346
Julliana Sousa <jullianasantossousa@hotmail.com>
2022-04-04 00:19:04 +0100

    Set active to pages when clicked

--- client/src/components/Product/TheNextPage.vue
+++ client/src/components/Product/TheNextPage.vue
@@ -2,14 +2,14 @@
 <div class="d-flex justify-content-center mt-5">
   <nav aria-label="page navigation example" >
     <ul class="pagination">
-      <li class="page-item">
-        <button class="page-link" @click='prevPage()'>
+      <li class="page-item disabled" id="previous">
+        <button type="button" class="page-link" @click='prevPage()'>
           <span aria-hidden="true">&laquo;</span>
           <span class="sr-only">Anterior</span>
         </button>
       </li>
-      <li class="page-item" v-for="n in 5" :key="n"><button class="page-link" @click='definePage(n)'> {{ n }} </button></li>
-      <li class="page-item">
+      <li class="page-item" v-for="n in 5" :key="n" :id="'page-' + n"><button class="page-link" @click='definePage(n)'> {{ n }} </button></li>
+      <li class="page-item" id="next">
         <button class="page-link" @click='nextPage()'>
           <span aria-hidden="true">&raquo;</span>
           <span class="sr-only">Seguinte</span>
@@ -28,22 +28,54 @@ export default {
       currentPage: 1,
     };
   },
+  mounted() {
+    this.setPageActive();
+  },
   methods: {
     prevPage() {
       if (this.currentPage > 1) {
         this.currentPage--;
         this.$emit("sendCurrentPage", this.currentPage);
+
+        this.manageDisable();
       }
     },
     nextPage() {
       if (this.currentPage < 5) {
         this.currentPage++;
         this.$emit("sendCurrentPage", this.currentPage);
+
+        this.manageDisable();
       }
     },
     definePage(num) {
+        var page = document.getElementById("page-" + this.currentPage);
+        page.classList.remove("active");
         this.currentPage = num;
+        page = document.getElementById("page-" + this.currentPage);
+        page.classList.add("active");
+        
         this.$emit("sendCurrentPage", this.currentPage);
+        this.manageDisable();
+    },
+    manageDisable() {
+      var previous = document.getElementById("previous");
+      var next = document.getElementById("next");
+      if (this.currentPage != 1) {
+        previous.classList.remove("disabled");
+      } else {
+        previous.classList.add("disabled");
+      }
+
+      if (this.currentPage == 5) {
+        next.classList.add("disabled");
+      } else {
+        next.classList.remove("disabled");
+      }
+    },
+    setPageActive() {
+      var initialPage = document.getElementById("page-1");
+      initialPage.classList.add("active");
     },
   },
 };
@@ -62,6 +94,12 @@ button:hover {
 button:focus {
   color: #608072;
   box-shadow: none;
+}
+
+.page-item.active .page-link {
+  color: #608072;
+  background-color: #ededed;
+  border: #dee2e6 1px solid;
 }
 
 </style>
