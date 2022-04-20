5a9214e4b4a78a353486169354a23b9cd000e37a
ObiMike16 <61832065+ObiMike16@users.noreply.github.com>
2022-04-20 16:03:37 +0100

    Changed navbar

--- client/src/components/Frontpage/TheNavbar copy.vue
+++ client/src/components/Frontpage/TheNavbar copy.vue
@@ -1,43 +1,46 @@
 <template>
 <nav>
-        <div class=" navmenu d-flex justify-content-center pt-2 pb-2">
-            <div class="align-self-center mt-2 mb-2">
+    <div class="container">
+        <div class="d-flex">
+            <div class="align-self-center pt-3">
                 <router-link to="/" class="navbar-brand">
                     <img alt="Logo do greenly" src="../../assets/logo_dark.png">
                 </router-link>
             </div>
-            <div class="input-group search-group align-self-center mt-2 mb-2 ms-4">
-                <input class="form-control" type="search" placeholder="" aria-label="Search" v-model="search">
-                <button class="btn btn-outline-success" type="submit" @click="submit(this.search)">Pesquisar</button>
+            <div class="flex-grow-1 input-group search-group align-self-center">
+                <input class="form-control" type="search" placeholder="" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Pesquisar</button>
             </div>
-            <div class="align-self-center text-uppercase nav-links mt-2 mb-2 ml-2 ms-5">
-                <router-link v-if="!userIsLoggedIn" to="/login">
-                    <font-awesome-icon :icon="['fas', 'user']" size="xl"/>
-                </router-link>
-                <router-link v-else to="/profile">
-                    <font-awesome-icon :icon="['fas', 'user']" size="xl"/>
+            <div v-if="!userIsLoggedIn" class="p-2 align-self-center pt-4 text-uppercase nav-links">
+                <router-link to="/login">
+                    Iniciar Sessão
                 </router-link>
             </div>
-            <div class="align-self-center nav-links mt-2 mb-2">
+            <div v-else class="p-2 align-self-center pt-4 text-uppercase nav-links">
+                <router-link to="/profile">
+                    {{ user.first_name + " " + user.last_name }}
+                </router-link>
+            </div>
+            <div class="p-2 align-self-center pt-4 text-uppercase nav-links">
                 <router-link to="/cart">
-                    <font-awesome-icon :icon="['fas', 'cart-shopping']" size="xl"/>
+                    <font-awesome-icon :icon="['fas', 'cart-shopping']" size="lg"/>
                 </router-link>
             </div>
         </div>
-        <div class=" navmenu2 d-flex justify-content-center pb-3 text-uppercase nav-links pt-2">
+        <div class="d-flex justify-content-center pb-3 text-uppercase nav-links pt-2">
             <div class="pt-2">
                 <router-link to="/promocoes" class="mb-2 pr-2">
-                    promoções <span class="ms-3"> | </span>            
+                    promoções
                 </router-link>
             </div>
             <div class="pt-2">
                 <router-link to="/produtos" class="mb-2 mr-2">
-                    produtos <span class="ms-3"> | </span> 
+                    produtos
                 </router-link>
             </div>
             <div class="pt-2">
                 <router-link to="/servicos" class="mb-2 ml-2">
-                    serviços <span class="ms-3"> | </span> 
+                    serviços
                 </router-link>
             </div>
             <div class="pt-2">
@@ -46,15 +49,14 @@
                 </router-link>
             </div>
         </div>
-    
+    </div>
 </nav>
 </template>
 
 <script>
 import { library } from '@fortawesome/fontawesome-svg-core';
-import { faCartShopping, faUser } from '@fortawesome/free-solid-svg-icons';
-
-library.add(faCartShopping, faUser);
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons';
+library.add(faCartShopping);
 
 import http from "../../../http-commmon"
 
@@ -64,17 +66,13 @@ export default {
         this.isLoggedIn();
         this.getUserInfo();
     },
-    data () {
+    data() {
         return {
-        search: '',
-        userIsLoggedIn: false,
-        user: []
-    }
+            userIsLoggedIn: false,
+            user: []
+        }
     },
     methods: {
-        submit(search) {
-            this.$emit('search-information', search);
-        },
         isLoggedIn() {
             let accessToken = JSON.parse(localStorage.getItem('accessToken'));
             if (accessToken){
@@ -103,40 +101,24 @@ export default {
                 })  
             }
         },
-    }
+    },
 };
 </script>
 
 <style scoped>
-    .navmenu {
+    nav {
         background-color: #608072;
         color: #fcfaf9;
     }
 
-    .navmenu2 {
-        background-color: #ffffff;
-        color: #000000;
-        box-shadow: 0 2px 3px rgb(40 40 40 / 20%);
-    }
-
-    .navmenu a {
+    nav a {
         color: #fcfaf9;
         text-decoration: none;
         margin-left: 20px;
     }
 
-    .navmenu2 a {
-        color: #000000;
-        text-decoration: none;
-        margin-left: 20px;
-
-    }
-
-    .navmenu a:hover {
-        color: #e4e4e4;
-    }
-    .navmenu2 a:hover {
-        color: #608072;
+    nav a:hover {
+        color: #dce5e1;
     }
 
     .nav-links {
@@ -153,7 +135,9 @@ export default {
     }
 
     .search-group {
-        width: 825px!important;
+        width: 570px;
+        margin-top: 2%;
+        margin-left: 5%;
     }
 
     input, button {
@@ -166,7 +150,6 @@ export default {
         background-color: #dce5e1;
         border: 0px;
         color: #608072;
-        box-shadow: none;
     }
 
     button:hover {
@@ -174,15 +157,4 @@ export default {
         border: 0px;
         color: #608072;
     }
-    .btn-outline-success{
-        box-shadow:none;
-    }
-    .form-control{
-        box-shadow:none;
-    }
-
-    .router-link-exact-active {
-       text-decoration: underline!important;
-    }
-    
 </style>
