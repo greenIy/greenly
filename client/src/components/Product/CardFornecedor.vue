0fd7b512f6b0eae3e744e950744714164d6e9638
Daniela Gonçalves <daniela.luis.goncalves@gmail.com>
2022-05-02 14:40:50 +0100

    fix positions supplier card

--- client/src/components/Product/CardFornecedor.vue
+++ client/src/components/Product/CardFornecedor.vue
@@ -3,13 +3,12 @@
     <label  @change="sendSuppliersSelected($event)">
       <div class="d-flex justify-content-between card-input">
         <div><p><font-awesome-icon class="fs-6 fa-fw mx-2" :icon="['fas', 'cubes']" />{{ supply.supplier.name }}</p></div>
-          <div class="mx-2"><input type="radio" name="fornecedorCard" v-model="fornecedorCard"  :value="supply.supplier.id" class="card-input-element"
-           /></div> 
+          <div class="mx-2"><input type="radio" name="fornecedorCard" v-model="fornecedorCard"  :value="supply.supplier.id" class="card-input-element"/></div> 
       </div>
       <div class="card-input mt-0">
         <p class="text-p"><b>Gastos:</b> {{ supply.warehouse.resource_usage }} kWh/kg</p>
         <p class="text-p"><b>Stock:</b> {{ supply.quantity }} produtos</p>
-        <p class="text-p"><b>Preço:</b> {{ supply.price }} €</p>
+        <p class="text-p"><b>Preço:</b> {{ supply.price }}€</p>
       </div>
     </label>
   </div>   
@@ -54,6 +53,5 @@ export default {
 .card{
   border: 1px solid #e5e5e5!important;
   width:15rem!important;
-  flex: unset;
 }
 </style>
