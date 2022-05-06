c2c83e78d5bf27d48a45308ede3ecbdf3c122309
Julliana Sousa <jullianasantossousa@hotmail.com>
2022-05-06 21:44:51 +0100

    Update http-common

--- client/http-common.js
+++ client/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://api.greenly.pt",
+  baseURL: "http://dev.greenly.pt",
   headers: {
     "Content-type": "application/json"
   }
