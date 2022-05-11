f3111b796480657ce532ff1c279a40cdb8996be8
xsu <estacontapertence@gmail.com>
2022-05-11 16:08:30 +0100

    Changed baseURL on client-side to use dev API server

--- client/http-common.js
+++ client/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:4000",
+  baseURL: "http://dev.greenly.pt",
   headers: {
     "Content-type": "application/json"
   }
