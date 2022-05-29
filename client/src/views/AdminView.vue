6f9cfd4012ee0867b9e7fa5f54c85b8e43e9a2d7
Lívia Batalha <liviaapbatalha@gmail.com>
2022-05-29 12:43:17 +0100

    Update AdminView.vue

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -217,10 +217,10 @@
                                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical icon-xxs"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg>
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="dropdownTeamOne">
-                                      <a class="dropdown-item" href="#">Action</a>
-                                      <a class="dropdown-item" href="#">Another action</a>
-                                      <a class="dropdown-item" href="#">Something else
-                                        here</a>
+                                      <a class="dropdown-item" href="#">Editar</a>
+                                      <a class="dropdown-item" href="#">Produtos</a>
+                                      <a class="dropdown-item" href="#">Armazéns</a>
+                                      <a class="dropdown-item" href="#">Remover</a>
                                     </div>
                                   </div>
                                 </td>
@@ -244,10 +244,9 @@
                                       <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-more-vertical icon-xxs"><circle cx="12" cy="12" r="1"></circle><circle cx="12" cy="5" r="1"></circle><circle cx="12" cy="19" r="1"></circle></svg>
                                     </a>
                                     <div class="dropdown-menu" aria-labelledby="dropdownTeamTwo">
-                                      <a class="dropdown-item" href="#">Action</a>
-                                      <a class="dropdown-item" href="#">Another action</a>
-                                      <a class="dropdown-item" href="#">Something else
-                                        here</a>
+                                      <a class="dropdown-item" href="#">Editar</a>
+                                      <a class="dropdown-item" href="#">Encomendas</a>
+                                      <a class="dropdown-item" href="#">Remover</a>
                                     </div>
                                   </div>
                                 </td>
@@ -272,10 +271,9 @@
                                     </a>
 
                                     <div class="dropdown-menu" aria-labelledby="dropdownTeamThree">
-                                      <a class="dropdown-item" href="#">Action</a>
-                                      <a class="dropdown-item" href="#">Another action</a>
-                                      <a class="dropdown-item" href="#">Something else
-                                        here</a>
+                                      <a class="dropdown-item" href="#">Editar</a>
+                                      <a class="dropdown-item" href="#">Veículos</a>
+                                      <a class="dropdown-item" href="#">Remover</a>
                                     </div>
                                   </div>
                                 </td>
