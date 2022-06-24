80891f792e81b7b8650344acee2c9500b7a48e17
sniparsdogg <estacontapertence@gmail.com>
2022-06-24 03:10:04 +0000

    testing prod environment

--- client/http-common.js
+++ client/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://dev.greenly.pt",
+  baseURL: "http://35.187.171.33:4000",
   headers: {
     "Content-type": "application/json"
   }
