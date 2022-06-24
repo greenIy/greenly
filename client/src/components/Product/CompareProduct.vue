48673e700a8179f772ac781849042eed8bee7b07
Daniela Gonçalves <daniela.luis.goncalves@gmail.com>
2022-06-24 19:52:02 +0100

    add comparation on large modal

--- client/src/components/Product/CompareProduct.vue
+++ client/src/components/Product/CompareProduct.vue
@@ -4,7 +4,7 @@
     <div @click="close" class="me-3 close">
         <font-awesome-icon class="fa-cog" :icon="['fa', 'close']" />
     </div>
-    <div class="row mt-4">
+    <div class="row mt-2">
         <div class="col-2">
         </div>
         <div class="col-10">
@@ -48,6 +48,7 @@
         </div>
         <div class="col-10">
             <p class="text"> COMPARAR PRODUTOS </p>
+         
         </div>
     </div>
      <div class="row mt-1">
@@ -57,17 +58,12 @@
             <div class="d-flex justify-content-center">
                 <div class="d-flex bd-highlight mb-3 me-4">
                     <div class="p-2 bd-highlight"><img class="img-fluid" src="../../assets/Team/daniela.jpg" alt="Fotografia da Daniela"></div>
-                    <div class="p-2 bd-highlight"><span class="text ms-2"><small>{{ productsToCompare[0].name }}</small></span> <div @click="remove(0)" class="p-2 bd-highlight">
-                    <font-awesome-icon class="fa-cog" :icon="['fa', 'trash']" size="xs" /> <small>Remover</small></div></div>
+                    <div class="p-2 bd-highlight"><span class="text ms-2"><small>{{ productsToCompare[0].name }}</small></span></div>
                 </div>
                 <div class="vl mb-4"></div>
-                <div class="d-flex bd-highlight mb-3 ms-5"  v-if="quantityP == 1">
-                    <p>Escolhe mais <b>1</b><br> para comparar </p>
-                </div>
                 <div v-if="productsToCompare.length == 2" class="d-flex bd-highlight mb-3 ms-4">
                     <div class="p-2 bd-highlight"><img class="img-fluid" src="../../assets/Team/daniela.jpg" alt="Fotografia da Daniela"></div>
-                    <div class="p-2 bd-highlight"><span class="text ms-2"><small>{{ productsToCompare[1].name }}</small></span> <div @click="remove(1)" class="p-2 bd-highlight">
-                    <font-awesome-icon class="fa-cog" :icon="['fa', 'trash']" size="xs" /> <small>Remover</small></div></div>
+                    <div class="p-2 bd-highlight"><span class="text ms-2"><small>{{ productsToCompare[1].name }}</small></span></div>
                 </div>
             </div>
         </div>
@@ -78,70 +74,79 @@
         <div class="col-2">
         </div>
         <div class="col-7">
+            <p class="text-muted"><small>São apresentadas as informações do Fornecedor e do Transportador mais sustentáveis para comparação.</small></p>
             <div class="d-flex justify-content-start">
                 <table class="table table-striped table-responsive">
                 <tbody>
-                    <tr>
-                    <th scope="col"><span class="d-flex align-items-center "><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'shopping-bag']" />Produto</small></span></th>
-                    <td scope="row"><span><small>Marcelo</small></span></td>
-                    <td scope="row"><span><small>Paulo</small></span></td>
+                    <tr class="font-compare">
+                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'user']" />Fornecedor</small></span></th>
+                    <td scope="row"><span><small>{{ productsToCompare[0].supplies[0].supplier.name}}</small></span></td>
+                    <td scope="row"><span><small>{{ productsToCompare[1].supplies[0].supplier.name}}</small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     </tr>
-                    <tr>
-                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'shopping-bag']" />Fornecedor</small></span></th>
-                    <td scope="row"><span><small>P1</small></span></td>
-                    <td scope="row"><span><small>P2</small></span></td>
-                     <td scope="row"><span><small></small></span></td>
+                    <tr class="font-compare">
+                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'sun']" />Recursos Renováveis</small></span></th>
+                    <td scope="row"><span><small>{{ productsToCompare[0].supplies[0].warehouse.renewable_resources}}%</small></span></td>
+                    <td scope="row"><span><small>{{ productsToCompare[1].supplies[0].warehouse.renewable_resources}}%</small></span></td>
                     <td scope="row"><span><small></small></span></td>
-                    <td scope="row"><span><small></small></span></td>
-                    </tr>
-                    <tr>
-                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'shopping-bag']" />Recursos Gastos</small></span></th>
-                    <td scope="row"><span><small>P1</small></span></td>
-                    <td scope="row"><span><small>P2</small></span></td>
-                     <td scope="row"><span><small></small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     </tr>
-                     <tr>
-                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'shopping-bag']" />Preço Fornecedor</small></span></th>
-                    <td scope="row"><span><small>P1</small></span></td>
-                    <td scope="row"><span><small>P2</small></span></td>
-                     <td scope="row"><span><small></small></span></td>
+                    <tr class="font-compare">
+                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'industry']" />Recursos de Armazenamento</small></span></th>
+                    <td scope="row"><span><small>{{ productsToCompare[0].supplies[0].warehouse.resource_usage}}kWh/dia</small></span></td>
+                    <td scope="row"><span><small>{{ productsToCompare[1].supplies[0].warehouse.resource_usage}}kWh/dia</small></span></td>
                     <td scope="row"><span><small></small></span></td>
-                    <td scope="row"><span><small></small></span></td>
-                    </tr>
-                    <tr>
-                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'shopping-bag']" />Transportadores</small></span></th>
-                    <td scope="row"><span><small>P1</small></span></td>
-                    <td scope="row"><span><small>P2</small></span></td>
-                     <td scope="row"><span><small></small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     </tr>
-                    <tr>
-                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'shopping-bag']" />Recursos Gastos</small></span></th>
-                    <td scope="row"><span><small>P1</small></span></td>
-                    <td scope="row"><span><small>P2</small></span></td>
-                     <td scope="row"><span><small></small></span></td>
+                    <tr class="font-compare">
+                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'money-bill-wave']" />Preço Fornecedor</small></span></th>
+                    <td scope="row"><span><small>{{ productsToCompare[0].supplies[0].price}}€</small></span></td>
+                    <td scope="row"><span><small>{{ productsToCompare[1].supplies[0].price}}€</small></span></td>
                     <td scope="row"><span><small></small></span></td>
-                    <td scope="row"><span><small></small></span></td>
-                    </tr>
-                     <tr>
-                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'shopping-bag']" />Preço Transportador</small></span></th>
-                    <td scope="row"><span><small>P1</small></span></td>
-                    <td scope="row"><span><small>P2</small></span></td>
-                     <td scope="row"><span><small></small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     </tr>
-                    <tr>
-                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'shopping-bag']" />Preço Total</small></span></th>
-                    <td scope="row"><span><small>P1</small></span></td>
-                    <td scope="row"><span><small>P2</small></span></td>
-                     <td scope="row"><span><small></small></span></td>
+                    <tr class="font-compare">
+                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'user']" />Transportador</small></span></th>
+                    <td scope="row"><span><small>{{ productsToCompare[0].supplies[0].transports[0].transporter.name}}</small></span></td>
+                    <td scope="row"><span><small>{{ productsToCompare[1].supplies[0].transports[0].transporter.name}}</small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    </tr>
+                    <tr class="font-compare">
+                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'skull-crossbones']" />Emissões Poluentes</small></span></th>
+                    <td scope="row"><span><small>{{ productsToCompare[0].supplies[0].transports[0].transporter.average_emissions}}CO₂ g/km/t</small></span></td>
+                    <td scope="row"><span><small>{{ productsToCompare[1].supplies[0].transports[0].transporter.average_emissions}}CO₂ g/km/t</small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    </tr>
+                    <tr class="font-compare">
+                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'gas-pump']" />Recursos de Transporte</small></span></th>
+                    <td scope="row"><span><small>{{ productsToCompare[0].supplies[0].transports[0].transporter.average_resource_usage}}l/100km</small></span></td>
+                    <td scope="row"><span><small>{{ productsToCompare[1].supplies[0].transports[0].transporter.average_resource_usage}}l/100km</small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    </tr>
+                    <tr class="font-compare">
+                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'money-bill-wave']" />Preço Transportador</small></span></th>
+                    <td scope="row"><span><small>{{ productsToCompare[0].supplies[0].transports[0].price}}€</small></span></td>
+                    <td scope="row"><span><small>{{ productsToCompare[1].supplies[0].transports[0].price}}€</small></span></td> 
+                    <td scope="row"><span><small></small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    <td scope="row"><span><small></small></span></td>
+                    </tr>
+                    <tr class="font-compare">
+                    <th scope="col"><span class="d-flex align-items-center"><small><font-awesome-icon class="fs-6 fa-fw me-1 text-muted" :icon="['fas', 'money-bill-wave']" />Preço Total</small></span></th>
+                    <td scope="row"><span><small>{{ productsToCompare[0].supplies[0].transports[0].price +  productsToCompare[0].supplies[0].price}}€</small></span></td>
+                    <td scope="row"><span><small>{{ productsToCompare[1].supplies[0].transports[0].price +  productsToCompare[1].supplies[0].price}}€</small></span></td>
+                    <td scope="row"><span><small></small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     <td scope="row"><span><small></small></span></td>
                     </tr>
@@ -158,9 +163,9 @@
 <script>
 
 import { library } from "@fortawesome/fontawesome-svg-core";
-import { faTrash, faClose} from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faClose, faSun, faIndustry, faMoneyBillWave, faSkullCrossbones, faGasPump } from "@fortawesome/free-solid-svg-icons";
 
-library.add(faTrash, faClose);
+library.add(faTrash, faClose, faSun, faIndustry, faMoneyBillWave, faSkullCrossbones, faGasPump);
 
 export default {
   name: "CompareProduct",
@@ -199,13 +204,13 @@ export default {
  background-color: white;
  z-index: 3;
  width:100%;
- height:17%;
+ height:19%;
 }
 .compare-modal{
  background-color: white;
  z-index: 3;
  width:100%;
- height:70%;
+ height:78%;
 }
 span:hover {
   color: #5e9f88;
@@ -224,5 +229,8 @@ img{
 }
 .vl {
   border-left: 1px solid #dbdad7;
+}
+.font-compare{
+    font-size:15px;
 }
 </style>
