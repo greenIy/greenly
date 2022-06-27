44fea99ba6177f3ce822fd923f12bf8535a0ea70
su <estacontapertence@gmail.com>
2022-06-27 11:01:17 +0100

    Changed default API URL

--- client/http-common.js
+++ client/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://35.187.171.33:4000",
+  baseURL: "http://api.greenly.pt:4000",
   headers: {
     "Content-type": "application/json"
   }
