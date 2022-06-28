29df795f4517078e6c0dab04130ecde333921db1
su <estacontapertence@gmail.com>
2022-06-28 20:30:44 +0100

    Removed SSL certs from vue config

--- client/vue.config.js
+++ client/vue.config.js
@@ -4,11 +4,6 @@ module.exports = {
     devServer: {
         port: 8080, // CHANGE YOUR PORT HERE!
 	allowedHosts: 'all'
-        /* https: {
-            key: fs.readFileSync('./certs/example.com+5-key.pem'),
-            cert: fs.readFileSync('./certs/example.com+5.pem'),
-            public: 'https://localhost:8080/'
-        } */
     },
     chainWebpack: config => {
 	config.optimization.sideEffects(false);
