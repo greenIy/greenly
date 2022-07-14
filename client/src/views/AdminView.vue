2794989d7b24600453b6b29072f9a63d31590886
Julliana Sousa <jullianasantossousa@hotmail.com>
2022-07-14 02:20:51 -0300

    98 admin graphs (#107)
    * Create chart component
    
    * WIP: add statistics to component
    
    * Add statistics to admin dashboard
    
    * Fixed http-common.js
    
    Co-authored-by: José Almeida <fc55373@alunos.fc.ul.pt>

--- client/src/views/AdminView.vue
+++ client/src/views/AdminView.vue
@@ -43,6 +43,33 @@
           <TheProductsTab :categories='this.categories' :products='this.products' :requests='this.requests' 
                           :amountCategories='this.amountCategories' :amountProducts='this.amountProducts' :amountRequests='this.amountRequests'/>
           <TheOrdersTab :orders='this.orders' :amountOrders='this.amountOrders' :amountRevenue='this.amountRevenue' :incrementRevenue='this.incrementRevenue' :amountProfit='this.amountProfit' :incrementProfit='this.incrementProfit' :amountSupplierResources='this.amountSupplierResources' :incrementSupplierResources='this.incrementSupplierResources' :amountTransporterResources='this.amountTransporterResources' :incrementTransporterResources='this.incrementTransporterResources' :amountEmissions='this.amountEmissions' :incrementEmissions='this.incrementEmissions' />
+          
+          <div class="row mt-4">
+                <div class="lc-block text-center">
+                  <div editable="rich">
+                      <h4 class="fw-bold display-2"  style="font-size: 40px !important;">Estatísticas</h4>
+                  </div>
+                </div>
+            </div>
+          <div v-if="loaded" class="container-fluid px-5 tab-pane fade show active" id="general-tab" role="tabpanel" aria-labelledby="general-pill">
+            <div class="row px-5 justify-content-md-center">
+              <div class="col-xl-4 col-lg-6 col-md-12 col-12 mt-5 stats p-2">
+                <Chart :informations='this.monthlyRevenue' :months='this.monthsNeeded' infoName="Vendas (€)"/>
+              </div>
+              <div class="col-xl-4 col-lg-6 col-md-12 col-12 mt-5 stats ms-2  p-2">
+               <Chart :informations='this.monthlyNewUsers' :months='this.monthsNeeded' infoName="Novos utilizadores"/>
+              </div>
+            </div>
+            <div class="row px-5 justify-content-md-center">
+              <div class="col-xl-4 col-lg-6 col-md-12 col-12 mt-5 stats  p-2">
+                <Chart :informations='this.monthlyEmissions' :months='this.monthsNeeded' infoName="Emissões (CO2 g/km/t)"/>
+              </div>
+              <div class="col-xl-4 col-lg-6 col-md-12 col-12 mt-5 stats ms-2  p-2">
+                <Chart :informations='this.monthlySupplyResourceUsage' :informationsPlus='this.monthlyTransportResourceUsage' :months='this.monthsNeeded' infoName="Recursos utilizados pelo fornecedor (kWh/dia)" infoNamePlus="Recursos utilizados pelo transportador (L/100km)"/>
+              </div>
+            </div>
+          </div>
+
         </div>
 
       </div>
@@ -63,6 +90,7 @@ import TheProductsTab from '@/components/Administration/Products/TheProductsTab.
 import TheOrdersTab from '@/components/Administration/Orders/TheOrdersTab.vue';
 import TheCategoryCreation from '../components/Administration/Products/TheCategoryCreation.vue';
 import TheProductCreation from '../components/Administration/Products/TheProductCreation.vue';
+import Chart from '../components/Administration/Chart.vue';
 
 import http from "../../http-common";
 
@@ -78,10 +106,12 @@ export default {
     TheOrdersTab,
     TheAdminRegistration,
     TheCategoryCreation,
-    TheProductCreation
+    TheProductCreation,
+    Chart
 },
   data() {
     return {
+      loaded: false,
       users: [],
       amountUsers: 0,
       usersIncrement: 0,
@@ -102,7 +132,25 @@ export default {
       amountTransporterResources: 0,
       incrementTransporterResources: 0,
       amountEmissions: 0,
-      incrementEmissions: 0
+      incrementEmissions: 0,
+      months: {"01": "Janeiro", 
+        "02" : "Fevereiro", 
+        "03": "Março",
+        "04": "Abril",
+        "05": "Maio",
+        "06": "Junho",
+        "07": "Julho",
+        "08": "Agosto",
+        "09": "Setembro",
+        "10": "Outubro",
+        "11": "Novembro",
+        "12": "Dezembro"},
+      monthsNeeded : [],
+      monthlyEmissions: [],
+      monthlyNewUsers: [],
+      monthlySupplyResourceUsage: [],
+      monthlyTransportResourceUsage: [],
+      monthlyRevenue: [],
     };
   },
   mounted() {
@@ -153,6 +201,23 @@ export default {
       this.incrementTransporterResources = response.data.resource_usage.last_month.transport.toFixed(2);
       this.amountEmissions = response.data.emissions.total.toFixed(2);
       this.incrementEmissions = response.data.emissions.last_month.toFixed(2);
+
+      this.manageStatistics(response.data.history);
+    },
+    manageStatistics(data) {
+      let filteredStatistics = data.slice(-6);
+      
+      for (let i = 0; i < filteredStatistics.length; i++) {
+        let d = filteredStatistics[i].date.split("-")[1];
+
+        this.monthsNeeded.push(this.months[d]);
+        this.monthlyEmissions.push(filteredStatistics[i].emissions);
+        this.monthlyNewUsers.push(filteredStatistics[i].new_users);
+        this.monthlySupplyResourceUsage.push(filteredStatistics[i].resource_usage.supply);
+        this.monthlyTransportResourceUsage.push(filteredStatistics[i].resource_usage.transport);
+        this.monthlyRevenue.push(filteredStatistics[i].revenue.total);
+      }
+      this.loaded = true;
     },
   }
 };
@@ -189,5 +254,10 @@ export default {
 
 .overlay {
   top: -150px;
+}
+
+.stats {
+  background-color: white;
+  border: 1px solid #d1d0d0;
 }
 </style>
